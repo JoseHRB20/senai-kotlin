8dbab44151882208426f7c94c7a6d6a5b8933a46
josehrb <jose.hrb20@hotmail.com>
2022-04-06 20:54:55 -0300

    Login com autenticador do firebase

--- app/src/main/java/com/example/kotlinSenai/MainActivity.kt
+++ app/src/main/java/com/example/kotlinSenai/MainActivity.kt
@@ -6,35 +6,71 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import com.example.firstapplication.R
+import android.widget.Toast
+import com.example.kotlinSenai.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnEnviar = findViewById<Button>(R.id.btnEnviar)
-        val btnNome = findViewById<Button>(R.id.btnNome)
-        val textViewNome = findViewById<TextView>(R.id.textViewNome)
-        val edtNome = findViewById<EditText>(R.id.edtNome)
 
-        btnNome.setOnClickListener {
-            if (edtNome.text.isNotBlank()) {
-                textViewNome.text = edtNome.text.toString()
+        val btnSignIn = findViewById<Button>(R.id.btnSignIn)
+        val txtSignUp = findViewById<TextView>(R.id.txtSignUp)
+        val edtUser = findViewById<EditText>(R.id.edtUser)
+        val edtPassword = findViewById<EditText>(R.id.edtPassword)
+
+        btnSignIn.setOnClickListener {
+            if (edtUser.text.toString().isEmpty() || edtPassword.text.toString().isEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Coloque um usuário e senha",
+                    Toast.LENGTH_SHORT
+                ).show()
             } else {
-                textViewNome.text = "Digite um nome abaixo"
+                val email: String = edtUser.text.toString().trim { it <= ' ' }
+                val password: String = edtPassword.text.toString().trim { it <= ' ' }
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(
+                        OnCompleteListener { task ->
+
+                            if (task.isSuccessful) {
+
+                                Toast.makeText(
+                                    this,
+                                    "Voçê está logado.",
+                                    Toast.LENGTH_LONG
+                                ).show()
+
+                                val intent = Intent(this, DrawerActivity::class.java)
+                                intent.putExtra(
+                                    "user_id",
+                                    FirebaseAuth.getInstance().currentUser!!.uid
+                                )
+                                intent.putExtra("email_id", email)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                Toast.makeText(
+                                    this,
+                                    task.exception!!.message.toString(),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        })
             }
         }
 
-        btnEnviar.setOnClickListener {
-            if (edtNome.text.isNotBlank()) {
-                val nomeDigitado = edtNome.text.toString()
-                val intent = Intent(this, ResultActivity::class.java)
-                intent.putExtra("NOME_DIGITADO", nomeDigitado)
-                startActivity(intent)
-            } else {
-                textViewNome.text = "Digite um nome abaixo"
-            }
+        txtSignUp.setOnClickListener {
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+
         }
     }
 }
